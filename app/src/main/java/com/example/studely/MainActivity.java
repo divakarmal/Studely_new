816b8c5db8eb249b62f1f9c3920b051555dc12ca
Divakar <malhotradivakar2000@gmail.com>
2020-06-08 22:04:41 +0800

    login and register page

--- app/src/main/java/com/example/studely/MainActivity.java
+++ app/src/main/java/com/example/studely/MainActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
