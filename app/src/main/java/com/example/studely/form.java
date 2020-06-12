a3eafd87a21abedba8ef71ccc197651739241f9a
Divakar <malhotradivakar2000@gmail.com>
2020-06-13 00:34:16 +0800

    drop down canteen implemented

--- app/src/main/java/com/example/studely/form.java
+++ app/src/main/java/com/example/studely/form.java
@@ -2,7 +2,9 @@ package com.example.studely;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,10 +17,17 @@ public class form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        field1 = findViewById(R.id.field1);
-        field2 = findViewById(R.id.field2);
-        field3 = findViewById(R.id.field3);
+        field1 = findViewById(R.id.phoneNumber);
+        field2 = findViewById(R.id.PrimaryAddress);
+        field3 = findViewById(R.id.PinCode);
         mSubmitBtn = findViewById(R.id.SubmitBtn);
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),HomeLanding.class));
+            }
+        });
 
     }
 }
