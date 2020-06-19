20547debe7ad272f35bb3918462de04d63defe22
Divakar <malhotradivakar2000@gmail.com>
2020-06-19 13:00:55 +0800

    Deliverer Posting functional

--- app/src/main/java/com/example/studely/DeliverOrderSelect.java
+++ app/src/main/java/com/example/studely/DeliverOrderSelect.java
@@ -15,12 +15,15 @@ public class DeliverOrderSelect extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliver_order_select);
         mNewPostingBtn = findViewById(R.id.newPosting);
+        final String canteenID = getIntent().getExtras().getString("canteenID");
 
 
         mNewPostingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),DeliverSelectTime.class));
+                Intent newIntent = new Intent(getApplicationContext(), DeliverSelectTime.class);
+                newIntent.putExtra("canteenID", canteenID);
+                startActivity(newIntent);
             }
         });
 
--- app/src/main/java/com/example/studely/DeliverSelectTime.java
+++ app/src/main/java/com/example/studely/DeliverSelectTime.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DeliverSelectTime extends AppCompatActivity {
     Button mConfirmBtn;
     TimePicker mTimePicker;
+    EditText mNoOfOrders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,25 @@ public class DeliverSelectTime extends AppCompatActivity {
         setContentView(R.layout.activity_deliver_select_time);
         mConfirmBtn = findViewById(R.id.confirm);
         mTimePicker = findViewById(R.id.timePicker1);
+        mNoOfOrders = findViewById(R.id.numOfOrders);
         mTimePicker.setIs24HourView(true);
+        final String canteenID = getIntent().getExtras().getString("canteenID");
+        final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        final String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         mConfirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),DeliverPostConfirm.class));
+                Intent newIntent = new Intent(getApplicationContext(), DeliverPostConfirm.class);
+                String mPushId = dbRef.push().getKey();
+                int clockTime = mTimePicker.getHour() * 100 + mTimePicker.getMinute();
+                String numOfOrders = mNoOfOrders.getText().toString();
+                String deliveryTime = Integer.toString(clockTime);
+                dbRef.child("DeliveryPostings").child(canteenID).child(mPushId).child("Deliverer").setValue(currentuser);
+                dbRef.child("DeliveryPostings").child(canteenID).child(mPushId).child("DeliveryTime").setValue(deliveryTime);
+                dbRef.child("DeliveryPostings").child(canteenID).child(mPushId).child("NoOfOrders").setValue(numOfOrders);
+
+                startActivity(newIntent);
             }
         });
     }
