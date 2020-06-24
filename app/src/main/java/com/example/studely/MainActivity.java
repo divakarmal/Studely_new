ce02a6325539ac9739e80b93f2f7b8a18d08cac1
Rahul0506 <rahul.gidijala@gmail.com>
2020-06-24 16:08:39 +0800

    Added auth persistence

--- app/src/main/java/com/example/studely/MainActivity.java
+++ app/src/main/java/com/example/studely/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.studely;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
     ImageButton proceed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         proceed = findViewById(R.id.nextmg);
 
         proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
+                FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        if (firebaseAuth.getCurrentUser() != null) {
+                            Log.d("AUTH", "User is logged in");
+                            startActivity(new Intent(MainActivity.this, WelcomePage.class));
+                        } else {
+                            Log.d("AUTH", "User is not logged in");
+                            startActivity(new Intent(MainActivity.this, Login.class));
+                        }
+                    }
+                });
             }
         });
     }
