52964b9e7e3fa1db8e3cb0162f159ff975a24383
Rahul0506 <rahul.gidijala@gmail.com>
2020-06-26 20:44:43 +0800

    Send name of deliverer to database when making postings

--- app/src/main/java/com/example/studely/DeliverSelectTime.java
+++ app/src/main/java/com/example/studely/DeliverSelectTime.java
@@ -7,9 +7,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DeliverSelectTime extends BottomNavBar {
     Button mConfirmBtn;
@@ -20,30 +25,47 @@ public class DeliverSelectTime extends BottomNavBar {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliver_select_time);
+        navBar(this.getApplicationContext());
+
         mConfirmBtn = findViewById(R.id.confirmBtn);
         mTimePicker = findViewById(R.id.timePicker1);
         mNoOfOrders = findViewById(R.id.numOfOrders);
         mTimePicker.setIs24HourView(true);
+
         final String canteenID = getIntent().getExtras().getString("canteenID");
         final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
         final String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        navBar(this.getApplicationContext());
+        final DatabaseReference userRef = dbRef.child("users").child(currentUser);
 
-        mConfirmBtn.setOnClickListener(new View.OnClickListener() {
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(getApplicationContext(), DeliverPostingConfirmed.class);
-                String pushID = dbRef.push().getKey();
-                int clockTime = mTimePicker.getHour() * 100 + mTimePicker.getMinute();
-                String numOfOrders;
-                numOfOrders = mNoOfOrders.getText().toString();
-                String deliveryTime = Integer.toString(clockTime);
-                dbRef.child("users").child(currentUser).child("DeliveryPostings").child(pushID).setValue(canteenID);
-                dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("Deliverer").setValue(currentUser);
-                dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("DeliveryTime").setValue(deliveryTime);
-                dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("NoOfOrders").setValue(numOfOrders);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final String name = (String) snapshot.child("name").getValue();
 
-                startActivity(newIntent);
+                mConfirmBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent newIntent = new Intent(getApplicationContext(), DeliverPostingConfirmed.class);
+                        String pushID = dbRef.push().getKey();
+                        int clockTime = mTimePicker.getHour() * 100 + mTimePicker.getMinute();
+                        String numOfOrders;
+                        numOfOrders = mNoOfOrders.getText().toString();
+                        String deliveryTime = Integer.toString(clockTime);
+                        dbRef.child("users").child(currentUser).child("DeliveryPostings").child(pushID).setValue(canteenID);
+                        dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("Deliverer").setValue(currentUser);
+                        dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("DeliveryTime").setValue(deliveryTime);
+                        dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("NoOfOrders").setValue(numOfOrders);
+                        dbRef.child("DeliveryPostings").child(canteenID).child(pushID).child("Name").setValue(name);
+
+                        startActivity(newIntent);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
