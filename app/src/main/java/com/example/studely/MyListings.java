4dfea360c5f4e105e3a83a970fff417971548044
Divakar <malhotradivakar2000@gmail.com>
2020-07-12 19:36:57 +0800

    buttons and edit texts designed

--- app/src/main/java/com/example/studely/MyListings.java
+++ app/src/main/java/com/example/studely/MyListings.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.studely.adapters.MyListingsAdapter;
 import com.google.firebase.auth.FirebaseAuth;
