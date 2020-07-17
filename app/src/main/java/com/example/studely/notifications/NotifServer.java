62e9e12b250ddab9decb09f9a23892be5a26db2f
Rahul0506 <rahul.gidijala@gmail.com>
2020-07-17 19:36:07 +0530

    Added app-side notification stuff

--- app/src/main/java/com/example/studely/notifications/NotifServer.java
+++ app/src/main/java/com/example/studely/notifications/NotifServer.java
@@ -3,6 +3,8 @@ package com.example.studely.notifications;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONObject;
+
 public class NotifServer {
     public static void sendRegistrationTokenToServer() {
 
@@ -13,11 +15,38 @@ public class NotifServer {
         DatabaseReference notifRef = db.getReference().child("notificationRequests");
 
         String pushID = notifRef.push().getKey();
-        notifRef.child(pushID).child("userTo").setValue(userTo);
-        notifRef.child(pushID).child("message").setValue(message);
+
+        try {
+            JSONObject json = new JSONObject();
+            JSONObject notif = new JSONObject();
+            notif.put("title", "Studely");
+            notif.put("body", message);
+            json.put("notification", notif);
+
+            notifRef.child(pushID).setValue(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void sendAwait(String userTo, String pushID) {
+    public static void sendAwait(String userTo, String delivID) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference notifRef = db.getReference().child("notificationRequests");
 
+        String pushID = notifRef.push().getKey();
+
+        try {
+            JSONObject json = new JSONObject();
+            JSONObject notif = new JSONObject();
+            notif.put("title", "Studely");
+            notif.put("body", "New delivery request");
+            json.put("notification", notif);
+            JSONObject data = new JSONObject();
+            data.put("pushID", delivID);
+            json.put("data", data);
+            notifRef.child(pushID).setValue(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
